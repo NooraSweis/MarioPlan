84497e3b60751fcb8fa21547bdcdd1125047a384
Noora Sweis <noora.sweis@gmail.com>
2021-04-06 21:00:18 +0300

    CSS finish

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           <Route path='/Project/:id' component={ProjectDetails}></Route>
           <Route path='/SignIn' component={SignIn}></Route>
           <Route path='/SignUp' component={SignUp}></Route>
+          <Route path='/create' component={CreateProject}></Route>
         </Switch>
       </BrowserRouter>
     );
